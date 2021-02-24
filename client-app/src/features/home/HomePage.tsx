c9ceb43c56839c982c47be2b8cd291780349d974
Henrikas Mockus <s031513@ad.viko.lt>
2021-02-24 23:25:47 +0200

    UI update

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Container } from 'semantic-ui-react'
+import { Button, Container, Header, Segment, Image } from 'semantic-ui-react'
 
 export default function HomePage(){
     return (
-        <Container style={{marginTop: '7em'}}>
-            <h1>zz</h1>
-            <h3>Go to <Link to='/activities'></Link></h3>
-        </Container>
+        <Segment inverted textAlign='center' vertical className='masthead'>
+            <Container>
+                <Header as='h1' inverted>
+                    <Image size='massive' src='/assets/logo.png' style={{marginBottom: 12}} />
+                    Reactivities
+                </Header>
+                <Header as='h2' inverted contex='Welcome to reactivities' />
+                <Button as={Link} to='/activities' size='huge' inverted>
+                    Take me to the activities!
+                </Button>
+            </Container>
+        </Segment>
     )
 }
